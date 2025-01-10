6c12f723b5cafd050558518c39179586da24bf1a
Andrii Stelmashenko <andrey.stelmashenko@gmail.com>
2025-01-10 13:41:10 +0200

    moved commands into api module

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
+pub mod api;
 #[warn(dead_code)]
 pub mod cli;
 pub mod config;
 
 use crate::cli::Cli;
+use api::command_deploy;
 use cli::Commands;
-// use query::*;
-use serde::{Deserialize, Serialize};
 use std::{error::Error, path::PathBuf};
 
 use clap::Parser;
-use config::{ConfVal, ViaxConfig};
+use config::ViaxConfig;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let user_dir = directories::UserDirs::new().unwrap();
@@ -32,74 +32,5 @@ fn main() -> Result<(), Box<dyn Error>> {
         None => {}
     }
 
-    // let q = FnMgmnt::build(FnMgmntVariables {
-    //     name: Some("my-fun"),
-    // });
-    // let response = req_client
-    //     .post("https://api.viax.lab.viax.tech/graphql")
-    //     .bearer_auth(format!("Bearer {}", viax_api_token.access_token))
-    //     .json(&q)
-    //     .send()
-    //     .unwrap();
-    // println!("response >>> {:#?}", response.text()?);
-
     Ok(())
-}
-
-fn command_deploy(
-    cfg: &ViaxConfig,
-    env_cfg: &ConfVal,
-    path: &PathBuf,
-) -> Result<(), Box<dyn Error>> {
-    let req_client = reqwest::blocking::Client::new();
-    let viax_api_token = acquire_token(
-        &env_cfg.auth_url,
-        &env_cfg.client_id,
-        &env_cfg.client_secret,
-        &cfg.realm,
-        &req_client,
-    );
-    // println!("api token: {:?}", viax_api_token);
-    // let viax_api_token = std::env::var("VIAX_API_TOKEN").expect("Missing VIAX_API_TOKEN env var");
-
-    let form = reqwest::blocking::multipart::Form::new()
-        .text("operations", r#"{ "operationName": "upsertFunction",  "query": "mutation upsertFunction($file: Upload!) { upsertFunction(input: { fun: $file }) { uid } }",  "variables": {    "file": null } }"#)
-        .text("map", r#"{ "File":["variables.file"] }"#)
-        .file("File", path)?;
-    //"/Users/inc/Work/viax/dev/viaxcli/fn.zip"
-    // println!("{:?}", form);
-
-    let response = req_client
-        .post("https://api.viax.lab.viax.tech/graphql")
-        .bearer_auth(format!("Bearer {}", viax_api_token.access_token))
-        .multipart(form)
-        .send()
-        .unwrap();
-    println!("response: {:#?}", response.text()?);
-    Ok(())
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ApiToken {
-    access_token: String,
-}
-
-fn acquire_token(
-    url: &str,
-    client_id: &str,
-    client_secret: &str,
-    realm: &str,
-    client: &reqwest::blocking::Client,
-) -> ApiToken {
-    let response = client
-        .post(format!(
-            "{url}/realms/{realm}/protocol/openid-connect/token",
-        ))
-        .form(&[
-            ("client_id", client_id),
-            ("client_secret", client_secret),
-            ("grant_type", "client_credentials"),
-        ])
-        .send();
-    response.unwrap().json().unwrap()
 }
